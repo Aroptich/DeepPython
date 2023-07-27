21b356bc76ddb708f3de1b55d21399d695323388
Alexsandr <artamoshkinas@gmail.com>
2023-07-27 20:20:44 +0700

    HomeWork3

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -1,6 +1,6 @@
-#Дан список повторяющихся элементов.
-#Вернуть список с дублирующимися элементами.
-#В результирующем списке не должно быть дубликатов. [1, 2, 3, 1, 2, 4, 5] -> [1, 2]
+# Дан список повторяющихся элементов.
+# Вернуть список с дублирующимися элементами.
+# В результирующем списке не должно быть дубликатов. [1, 2, 3, 1, 2, 4, 5] -> [1, 2]
 from collections import Counter
 from typing import Any
 
@@ -11,12 +11,12 @@ def list_of_repeating_elements(element: list[Any]) -> list[Any]:
     return list(set([el for el in element if element.count(el) > 1]))
 
 
-#В большой текстовой строке подсчитать количество встречаемых слов и вернуть 10 самых частых.
-#Не учитывать знаки препинания и регистр символов.
-#За основу возьмите любую статью из википедии или из документации к языку.
-#(Может помочь метод translate из модуля string)
+# В большой текстовой строке подсчитать количество встречаемых слов и вернуть 10 самых частых.
+# Не учитывать знаки препинания и регистр символов.
+# За основу возьмите любую статью из википедии или из документации к языку.
+# (Может помочь метод translate из модуля string)
 
-def main(text: str, count: int) ->list[tuple[str,int]]:
+def main(text: str, count: int) -> list[tuple[str, int]]:
     """Функция принимает путь к файлу и количество вывода повторений.
     Возвращает список кортежей с повторяющимися элементами"""
     try:
@@ -31,19 +31,19 @@ def reading_text(text: str) -> list[str]:
     try:
         if not isinstance(text, str):
             raise TypeError("Аргумент должен быть строкового типа")
-        with open(text, 'r', encoding='utf-8')  as file:
-            return file.read().split()
+        with open(text, 'r', encoding='utf-8') as file:
+            return file.read().lower().split()
     except Exception as err:
         print(err)
         return -1
 
 
-#Создайте словарь со списком вещей для похода в качестве ключа и их массой в качестве значения.
-#Определите какие вещи влезут в рюкзак передав его максимальную грузоподъёмность.
-#Достаточно вернуть один допустимый вариант.
-#*Верните все возможные варианты комплектации рюкзака.
+# Создайте словарь со списком вещей для похода в качестве ключа и их массой в качестве значения.
+# Определите какие вещи влезут в рюкзак передав его максимальную грузоподъёмность.
+# Достаточно вернуть один допустимый вариант.
+# *Верните все возможные варианты комплектации рюкзака.
 
-def backpack(capacity: int|float) -> dict:
+def backpack(capacity: int) -> dict:
     THINGS = {'яблоко': 0.1,
               'нож': 0.5,
               'коструля': 1.0,
@@ -56,6 +56,20 @@ def backpack(capacity: int|float) -> dict:
               'фонарик': 1.0,
               'мясо': 3.0,
               'аптечка': 1.5}
+    cargo = []
+    lst_things = []
+    for thing in THINGS:
+        if THINGS[thing] <= capacity and (sum(cargo) + THINGS[thing]) <= capacity:
+            cargo.append(THINGS[thing])
+            lst_things.append(thing)
+    print(lst_things)
+
+
+
+
 
 if __name__ == '__main__':
+    print(list_of_repeating_elements([1, 2, 3, 1, 2, 4, 5]))
     print(main('text.txt', 10))
+    backpack(5)
+
